738729161227db9e5827e140cbeee30564caa2dc
홍채원 <sophia1919@naver.com>
2026-02-01 04:42:43 +0900

    Title: 특정 조건 두 정수 비교, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260201/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -0,0 +1,11 @@
+A,B=map(int,input().split())
+
+if A<B:
+    print(1, sep=" ", end=" ")
+else:
+    print(0,sep=" ",  end=" ")
+
+if A==B:
+    print(1,sep=" ", end=" ")
+else:
+    print(0,sep=" ", end=" ") 
