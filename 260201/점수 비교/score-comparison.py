f09fe766cb030c841cb29f6c8b1fdafbe0a173ea
홍채원 <sophia1919@naver.com>
2026-02-01 20:52:30 +0900

    Title: 점수 비교, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 260201/점수 비교/score-comparison.py
+++ 260201/점수 비교/score-comparison.py
@@ -1,11 +1,7 @@
 a_math, a_english = map(int, input().split())
 b_math, b_english = map(int, input().split())
 
-A = a_math, a_english
-B = b_math, b_english
-
-
-if A > B:
+if (a_math) > (b_math) and (a_english) >(b_english):
     print(1)
 else:
     print(0)
