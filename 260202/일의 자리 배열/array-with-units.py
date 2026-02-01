d95a5e10de9f0de4201aed770f452239f50fa262
홍채원 <sophia1919@naver.com>
2026-02-02 01:47:15 +0900

    Title: 일의 자리 배열, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260202/일의 자리 배열/array-with-units.py
+++ 260202/일의 자리 배열/array-with-units.py
@@ -1,3 +1,4 @@
+
 pivot = list(map(int, input().split()))
 
 for _ in range(10):
@@ -5,4 +6,4 @@ for _ in range(10):
     num = num % 10
     pivot.append(num)
 # num에서 추가하고 계산된 값을 pivot에 넣어 출력한다
-print(num)
+print(*pivot)
