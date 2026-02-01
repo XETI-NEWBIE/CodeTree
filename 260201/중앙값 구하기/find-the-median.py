b742f47b87600661e24f5299bea90fdf2dee312b
홍채원 <sophia1919@naver.com>
2026-02-01 22:30:53 +0900

    Title: 중앙값 구하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260201/중앙값 구하기/find-the-median.py
+++ 260201/중앙값 구하기/find-the-median.py
@@ -1,10 +1,10 @@
 A,B,C=map(int, input().split())
 
-if (A<B<C) and (C<B<A):
+if (A<B<C) or (C<B<A):
     print(B)
-elif (B<A<C) and (C<A<B):
+elif (B<A<C) or (C<A<B):
     print(A)
-elif (A<C<B) and (B<C<A):
+elif (A<C<B) or (B<C<A):
     print(C)
 else:
     print(None)
