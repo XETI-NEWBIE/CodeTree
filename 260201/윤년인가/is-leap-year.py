b53190c757e2b78aebae69144e6898ab3bf693b1
홍채원 <sophia1919@naver.com>
2026-02-01 21:52:15 +0900

    Title: 윤년인가, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260201/윤년인가/is-leap-year.py
+++ 260201/윤년인가/is-leap-year.py
@@ -4,9 +4,10 @@ Y=int(input())
 Leap = "true"
 Common = "false"
 
-if Y%4==0:
+
+if Y%4==0 and Y%100==0 and Y%400!=0:
+    print(Common)
+elif Y%4==0:
     print(Leap)
-    if Y%100==0 and Y%400!=0:
-        print(Common)
 else:
     print(Common)
