38871ab8a298fcd682ebd50c997893952c37bb56
홍채원 <sophia1919@naver.com>
2026-02-03 11:14:23 +0900

    Title: 100점 도달하기, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260203/100점 도달하기/reach-100-point.py
+++ 260203/100점 도달하기/reach-100-point.py
@@ -1,14 +1,16 @@
 scores = list(map(int, input().split()))
+
 for num in scores:
-    if num>=90:
-     	print("A")
-	elif num>=80:
-     	print("B")
-    elif num>=70:
-     	print("C")
-    elif num>=60:
-     	print("D")
+    if num >= 90:
+        print("A")
+    elif num >= 80:
+        print("B")
+    elif num >= 70:
+        print("C")
+    elif num >= 60:
+        print("D")
     else:
-   	  	print("F")
-       
+        print("F")
+        
+# 마지막에 숫자를 찍고 싶다면 for문 밖에서 num 출력
 print(num, end=" ")
