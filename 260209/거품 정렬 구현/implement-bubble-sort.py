74c3256abaaf6accbc22c3b747d1371f528c393d
홍채원 <sophia1919@naver.com>
2026-02-09 02:18:50 +0900

    Title: 거품 정렬 구현, Time: 64ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260209/거품 정렬 구현/implement-bubble-sort.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+def bubble_sort(a,N):
+    for i in range(N-1,0,-1):
+        for j in range(i):
+            if a[j]>a[j+1]:
+                a[j], a[j+1] = a[j+1], a[j]
+
+bubble_sort(arr,n)
+print(*arr)
