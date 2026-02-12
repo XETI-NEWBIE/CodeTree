9b833ac6e41a7a001de7880d810f8c770c3cb501
홍채원 <sophia1919@naver.com>
2026-02-12 16:10:15 +0900

    Title: 정수의 조건 여부 3, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260212/정수의 조건 여부 3/numbers-condition-3.py
+++ 260212/정수의 조건 여부 3/numbers-condition-3.py
@@ -1,7 +1,7 @@
 a = int(input())
 
-for i in (13,19):
-    if a%13==0 or a%19==0:
-        print("True")
-    else:
-        print("False")
+# for i in (13,19):
+if a%13==0 or a%19==0:
+    print(bool(1))
+else:
+    print(bool(0))
