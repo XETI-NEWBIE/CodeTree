dd030399ee22d37a0c692eef5b80c3ad370e1340
홍채원 <sophia1919@naver.com>
2026-02-24 02:12:12 +0900

    Title: 최댓값 구하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260224/최댓값 구하기/maximum-value.py
+++ 260224/최댓값 구하기/maximum-value.py
@@ -1,7 +1,16 @@
-a,b,c = input().split()
-max_val=0
+a,b,c = map(int, input().split())
+max_val=a
 
-for i in a,b,c:
-    if i>=i[-1]:
+for i in [a,b,c]:
+    if i>=max_val:
         max_val=i
 print(max_val)   
+
+# a, b, c = map(int, input().split()) # 정수로 변환
+# max_val = a # 일단 첫 번째 숫자가 가장 크다고 가정
+
+# for i in [a, b, c]:
+#     if i > max_val: # 현재 들고 있는 최대값보다 i가 더 크면?
+#         max_val = i # 최대값을 i로 교체!
+        
+# print(max_val)
