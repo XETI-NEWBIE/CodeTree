58218d72a500a7558d40ff39af678a27a57906e0
홍채원 <sophia1919@naver.com>
2026-02-24 02:44:09 +0900

    Title: 중앙값 구하기, Time: 61ms, Memory: 16MB, Status: Passed - Codetree

--- 260224/중앙값 구하기/find-the-median.py
+++ 260224/중앙값 구하기/find-the-median.py
@@ -1,20 +1,22 @@
-a,b,c = map(int,input().split())
+a,b,c=map(int,input().split())
 
-if a>=b:
-    if a<=c:
-        print(a)
-if a<b:
-    if a>=c:
-        print(a)
-if b>=c:
-    if b<=a:
-        print(b)
-if b<c:
-    if b>=a:
-        print(b)
-if c>=a:
-    if c<=b:
-        print(c)
-if c<b:
-    if c>=a:
-        print(c)
+if (a<=b<=c) or (c<=b<=a):
+    print(b)
+elif (b<=a<=c) or (c<=a<=b):
+    print(a)
+else:
+    print(c)
+
+
+
+# a, b, c = map(int, input().split())
+
+# # a가 중앙값인 경우: b <= a <= c 또는 c <= a <= b
+# if (b <= a <= c) or (c <= a <= b):
+#     print(a)
+# # b가 중앙값인 경우: a <= b <= c 또는 c <= b <= a
+# elif (a <= b <= c) or (c <= b <= a):
+#     print(b)
+# # 위 두 경우가 아니면 당연히 c가 중앙값!
+# else:
+#     print(c)
