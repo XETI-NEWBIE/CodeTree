ccba2abb9902c4a9ee0efcc26400e40f30ef83f4
홍채원 <sophia1919@naver.com>
2026-03-09 10:40:42 +0900

    Title: 몫 출력하기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260309/몫 출력하기/print-share.py
+++ 260309/몫 출력하기/print-share.py
@@ -1,11 +1,15 @@
 
+cnt = 0
 
-for _ in range(4):
+while cnt < 3:
     n=int(input())
 
     if n%2==0:
         n=n//2
         print(n)
+        cnt+=1
 
-    if n%2==1:
-        continue
+    # if n%2==1:
+    #     continue
+    #     cnt+=1
+    
